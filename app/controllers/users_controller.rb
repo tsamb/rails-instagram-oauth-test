2d9dcc54b31820d2efbd0e3567a262c229c423b9
Sam Blackman <sblackman@gmail.com>
2015-07-16 10:37:16 -0400

    add instagram gem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
   def oauth_connect
-    redirect_to Instagram.authorize_url(:redirect_uri => "http://localhost:3000/oauth_callback")
+    redirect_to Instagram.authorize_url(:redirect_uri => "https://evening-brook-9922.herokuapp.com/oauth_callback")
   end
 
   def oauth_callback
-    response = Instagram.get_access_token(params[:code], :redirect_uri => "http://localhost:3000/oauth_callback")
+    response = Instagram.get_access_token(params[:code], :redirect_uri => "https://evening-brook-9922.herokuapp.com/oauth_callback")
     @user = User.find_by(instagram_token: response.access_token)
     @user = User.create(instagram_token: response.access_token) unless @user
     render :index
