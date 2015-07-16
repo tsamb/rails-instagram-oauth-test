47edd025f91161115e915da9ad716163894c7b60
Sam Blackman <sblackman@gmail.com>
2015-07-16 10:57:25 -0400

    change root url

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
   def oauth_connect
-    redirect_to Instagram.authorize_url(:redirect_uri => "https://evening-brook-9922.herokuapp.com/oauth_callback")
+    redirect_to Instagram.authorize_url(:redirect_uri => "https://floating-wave-4718.herokuapp.com/oauth_callback")
   end
 
   def oauth_callback
-    response = Instagram.get_access_token(params[:code], :redirect_uri => "https://evening-brook-9922.herokuapp.com/oauth_callback")
+    response = Instagram.get_access_token(params[:code], :redirect_uri => "https://floating-wave-4718.herokuapp.com/oauth_callback")
     @user = User.find_by(instagram_token: response.access_token)
     @user = User.create(instagram_token: response.access_token) unless @user
     render :index
